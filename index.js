c50f8a91edeecfc8fa9dac44de727a33272004cf
Aniket Pratyush <51285661+lucifer-199@users.noreply.github.com>
2020-12-10 17:20:53 +0530

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+// // 1. Ways to print
+// console.log("Bol Na");
+// alert("Hello");
+// document.write("Hello");
+// 2. JavaScript Console API
+// console.log("Hello");
+// console.warn("Chala Jaa");
+// console.error("Bhakk");
+// let mydate= new Date();
+// console.log(mydate.getDate());
+// console.log(mydate.getMinutes());
+// console.log(mydate.getHours());
+// let x=document.getElementsByTagName('div');
+// createy=document.createElement('p');
+// createy.innerText= "This is para";
+// x[0].appendChild(createy);
+const test= "Hello";
+const isAll= false;
+if(test===test.toUpperCase()){
+    isAll=true;
+}
+console.log("Hello");
---
+++ practice.js
@@ -0,0 +1,6 @@
+const test= "Hello";
+const isAll= false;
+if(test===test.toUpperCase()){
+    isAll=true;
+}
+console.log("Hello");
